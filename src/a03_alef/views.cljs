dc87c6605ba086358bf2f36abf87ab6fb8f14a79
kees- <6820950+kees-@users.noreply.github.com>
2022-02-17 15:02:47 -0800

    Create testing ground with paths
    The problem: reloading the page reloads the state.

--- src/a03_alef/views.cljs
+++ src/a03_alef/views.cljs
@@ -7,6 +7,22 @@
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
-    [:div
+    [:div.i
      [:h1
-      "Hello from " @name]]))
+      "Hello from " @name]
+     [:div
+      [:input {:type :text
+               :value @(re-frame/subscribe [::subs/path-box-input])
+               :on-change #(re-frame/dispatch [::events/path-box-input
+                                               (.. % -target -value)])}]
+      [:input {:type :button
+               :value "Submit path"
+               :on-click #(re-frame/dispatch
+                           [::events/boop
+                            @(re-frame/subscribe [::subs/path-box-input])
+                            :children])}]]
+     [:div
+      [:ul.b
+       (for [li (-> @(re-frame/subscribe [::subs/children])
+                    keys)]
+         [:li {:key li} (.toUpperCase li)])]]]))
