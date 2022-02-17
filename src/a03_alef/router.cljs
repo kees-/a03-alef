9f3e439f86ba477b2741bf21a9e1ce4233b0ede4
kees- <6820950+kees-@users.noreply.github.com>
2022-02-17 14:57:53 -0800

    Create a static test route tree

--- src/a03_alef/router.cljs
+++ src/a03_alef/router.cljs
@@ -1,2 +1,12 @@
 (ns a03-alef.router
   (:require [cemerick.url :as url]))
+
+(def routes
+  {"a" {:content "My acka acka"
+        :children {"x" :this
+                   "y" :that
+                   "z" :then}}
+   "b" {:content "Baca baca.. taca"
+        :children {"u" :though
+                   "v" :there
+                   "w" :these}}})
