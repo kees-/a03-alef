9a00447b4defdeea43ffb6435779f9901fa241f0
kees- <6820950+kees-@users.noreply.github.com>
2022-02-17 14:57:21 -0800

    Add requirements

--- src/a03_alef/views.cljs
+++ src/a03_alef/views.cljs
@@ -1,7 +1,9 @@
 (ns a03-alef.views
   (:require
    [re-frame.core :as re-frame]
-   [a03-alef.subs :as subs]))
+   [a03-alef.subs :as subs]
+   [a03-alef.events :as events]
+   [a03-alef.router :as router]))
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
