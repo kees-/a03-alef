a56e9325f8669b61871a5ba33c6cec3b88ed2b5f
kees- <6820950+kees-@users.noreply.github.com>
2022-02-17 14:58:56 -0800

    Add requirements

--- src/a03_alef/events.cljs
+++ src/a03_alef/events.cljs
@@ -1,7 +1,8 @@
 (ns a03-alef.events
   (:require
    [re-frame.core :as re-frame]
-   [a03-alef.db :as db]))
+   [a03-alef.db :as db]
+   [a03-alef.router :as router]))
 
 (re-frame/reg-event-db
  ::initialize-db
