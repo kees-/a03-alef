1c490c3849ded5e04e18428fb0d01d027e220e48
kees- <6820950+kees-@users.noreply.github.com>
2022-02-18 08:36:49 -0800

    Refactor panel fns

--- src/a03_alef/views.cljs
+++ src/a03_alef/views.cljs
@@ -5,24 +5,28 @@
    [a03-alef.events :as events]
    [a03-alef.router :as router]))
 
+(defn input-panel
+  [value]
+  [:div.panel
+   [:input {:type :text
+            :value value
+            :on-change #(re-frame/dispatch [::events/path-box-input
+                                            (.. % -target -value)])}]
+   [:input {:type :button
+            :value "Submit path"
+            :on-click #(re-frame/dispatch
+                        [::events/boop value :children])}]])
+
+(defn list-children
+  []
+  [:div>ul.b
+   (for [li (-> @(re-frame/subscribe [::subs/children]) keys)]
+     [:li {:key li} (.toUpperCase li)])])
+
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
+  (let [name (re-frame/subscribe [::subs/name])
+        value (re-frame/subscribe [::subs/path-box-input])]
     [:div.i
-     [:h1
-      "Hello from " @name]
-     [:div
-      [:input {:type :text
-               :value @(re-frame/subscribe [::subs/path-box-input])
-               :on-change #(re-frame/dispatch [::events/path-box-input
-                                               (.. % -target -value)])}]
-      [:input {:type :button
-               :value "Submit path"
-               :on-click #(re-frame/dispatch
-                           [::events/boop
-                            @(re-frame/subscribe [::subs/path-box-input])
-                            :children])}]]
-     [:div
-      [:ul.b
-       (for [li (-> @(re-frame/subscribe [::subs/children])
-                    keys)]
-         [:li {:key li} (.toUpperCase li)])]]]))
+     [:h1 "Hello from " @name]
+     (input-panel @value)
+     (list-children)]))
