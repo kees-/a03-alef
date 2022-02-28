db1a70ae1776f45e20ebfab23ace6e0f8776620b
kees- <6820950+kees-@users.noreply.github.com>
2022-02-28 12:24:01 -0800

    Add an ajax get request fn

--- src/a03_alef/data.cljs
+++ src/a03_alef/data.cljs
@@ -5,3 +5,23 @@
    [ajax.core :as ajax]
    [ajax.edn :as edn]))
 
+(defn handler
+  [response]
+  (re-frame/dispatch [::events/base-content response]))
+
+(defn error-handler
+  [response]
+  (cljs.pprint/pprint "The request failed. Response:")
+  (cljs.pprint/pprint response))
+
+(defn callback
+  []
+  (re-frame/dispatch [::events/initialize-focus]))
+
+(defn load!
+  [source]
+  (ajax/GET source
+            {:response-format (edn/edn-response-format)
+             :handler handler
+             :error-handler error-handler
+             :finally callback}))
